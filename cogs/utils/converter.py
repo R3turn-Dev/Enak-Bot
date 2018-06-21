79bf6143332cb8359d162b8bcc1b10deaf2805b8
return0927 <bjleh0927@gmail.com>
2018-06-21 22:00:16 +0900

    * 2018.06.21 컨버터 DB Command Info -> File Definition

--- cogs/utils/converter.py
+++ cogs/utils/converter.py
@@ -1,3 +1,35 @@
+from Enak.db import PostgreSQL
+from Enak.settings import SettingManager
+
+
+conf = SettingManager().get().PostgreSQL
+DB = PostgreSQL(**conf)
+
+
+_FORMAT = """
 from discord.ext import commands
+from random import choice
 
 
+class {server_ident}:
+    def __init__(self, bot):
+        self.bot = bot
+        self.sessions = set()
+
+{commands}
+
+def setup(bot):
+    bot.add_cog({server_ident}(bot))
+"""
+
+_COMMAND_FORMAT = """
+    @commands.command('{command.present}', pass_context=True)
+    async def {command.global}(self, ctx, *args, **kwargs):
+        stack = {
+            "ㅎㅇㅎㅇ",
+            "으낙입니다",
+            "그러는 당신은?"
+        ]
+
+        await self.bot.send_message(ctx.message.channel, choice(stack))
+"""
