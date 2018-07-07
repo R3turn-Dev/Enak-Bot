0f6022b685e8b51d35958f46d14678998c8f8872
return0927 <bjleh0927@gmail.com>
2018-07-08 02:48:48 +0900

    * 2018.07.08 Moved configuring bot into a single bot

--- Twitch/Enak/__init__.py
+++ Twitch/Enak/__init__.py
@@ -1,1 +1,28 @@
 from .twitch import TwichClient
+from .settings import Config
+
+
+default_cogs = [
+    'cogs.basic_commands',
+    'cogs.console_logger'
+]
+
+
+class Bot(TwichClient):
+    def __init__(self, *args, **kwargs):
+        self.config = Config()
+        print(self.config.get("Bot.username"))
+        print(self.config.get("Bot.OAuth_token"))
+        print(self.config.get("Bot.channels"))
+        self.user = kwargs.get("user", self.config.get("Bot.username"))
+        self.token = kwargs.get("token", self.config.get("Bot.OAuth_token"))
+        self.channels = kwargs.get("channels", [v.strip() for v in self.config.get("Bot.channels").split(",")])
+
+        kwargs['user'] = self.user
+        kwargs['token'] = self.token
+        kwargs['channels'] = self.channels
+
+        super().__init__(*args, **kwargs)
+
+        for cog in default_cogs:
+            self.load_extension(cog)
--- Twitch/bot.py
+++ Twitch/bot.py
@@ -1,25 +1,6 @@
-from Enak import TwichClient
-from Enak.settings import Config
+from Enak import Bot
 
-def handler(*args):
-    print(args)
-
-def message(b_msg):
-    print(b_msg.decode())
-
-config = Config()
-name = config.get("Credentials.username")
-token = config.get("Credentials.OAuth_token")
-
-print(
-    f"""
-    Accessing Twitch IRC
-
-    [ Credentials ]
-    User  : {name}
-    Token : {token[:5]}*******************************
-    """
+client = Bot(
+    host="irc.twitch.tv"
 )
-
-client = TwichClient(host="irc.twitch.tv", name = name, token = token, on_open = handler, on_close = handler, on_data = message)
 client.run()
