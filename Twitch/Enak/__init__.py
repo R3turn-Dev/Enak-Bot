d42c875ff2adc46de8fa90eb487b539bde52c34d
return0927 <bjleh0927@gmail.com>
2018-07-08 19:01:14 +0900

    * 2018.07.08 Added APIHandler to TwitchBot
     - Changed OAuth_token into csc
     - Added cid (Twitch Client-ID)
     - Moved default_cogs into class `Enak.Bot`

--- Twitch/Enak/__init__.py
+++ Twitch/Enak/__init__.py
@@ -3,7 +3,6 @@ from .settings import Config
 
 
 default_cogs = [
-    'cogs.basic_commands',
     'cogs.console_logger'
 ]
 
@@ -11,18 +10,18 @@ default_cogs = [
 class Bot(TwichClient):
     def __init__(self, *args, **kwargs):
         self.config = Config()
-        print(self.config.get("Bot.username"))
-        print(self.config.get("Bot.OAuth_token"))
-        print(self.config.get("Bot.channels"))
         self.user = kwargs.get("user", self.config.get("Bot.username"))
-        self.token = kwargs.get("token", self.config.get("Bot.OAuth_token"))
+        self.cid = kwargs.get("token", self.config.get("Bot.Client-ID"))
+        self.csc = kwargs.get("token", self.config.get("Bot.Client-Secret"))
         self.channels = kwargs.get("channels", [v.strip() for v in self.config.get("Bot.channels").split(",")])
+        self._default_cogs = kwargs.get("cogs", default_cogs)
 
         kwargs['user'] = self.user
-        kwargs['token'] = self.token
+        kwargs['cid'] = self.cid
+        kwargs['csc'] = self.csc
         kwargs['channels'] = self.channels
 
         super().__init__(*args, **kwargs)
 
-        for cog in default_cogs:
+        for cog in self._default_cogs:
             self.load_extension(cog)
--- Twitch/bot.py
+++ Twitch/bot.py
@@ -1,6 +1,11 @@
 from Enak import Bot
 
+extensions = [
+    "cogs.basic_commands"
+]
+
 client = Bot(
-    host="irc.twitch.tv"
+    host = "irc.twitch.tv",
+    cogs = extensions
 )
 client.run()
