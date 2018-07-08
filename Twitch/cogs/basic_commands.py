b1f2bdc79f740ed81f12c31ab7910f17e037ee3b
return0927 <bjleh0927@gmail.com>
2018-07-08 19:34:12 +0900

    * 2018.07.08 Built default-cog
     - Built basic_commands including common commands
     - Built console_logger for logging on console window

---
+++ Twitch/cogs/basic_commands.py
@@ -0,0 +1,53 @@
+import aiohttp
+from Enak.prototypes import command
+
+
+class BasicCommands:
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = set()
+
+
+    @command(['uptime', '업타임'], pass_context = True)
+    async def uptime(self, ctx, *_):
+        bot = ctx.bot
+        API = bot.APIHandler
+
+        user = await API.get_user_by_name(ctx.channel.name)
+        uptime = await API.get_uptime(user.tid)
+        await ctx.reply(f"업타임: {uptime}")
+
+    @command(['title', '방제'], pass_context=True)
+    async def title(self, ctx, *_):
+        API = ctx.bot.APIHandler
+
+        user = await API.get_user_by_name(ctx.channel.name)
+        stream = await API.get_stream(user.tid)
+        if stream:
+            await ctx.reply(f"방제: {stream['title']}")
+        else:
+            await ctx.reply("방송중이 아닙니다.")
+
+    @command(['game', '게임'], pass_context=True)
+    async def game(self, ctx, *_):
+        API = ctx.bot.APIHandler
+
+        user = await API.get_user_by_name(ctx.channel.name)
+        stream = await API.get_stream(user.tid)
+        if stream:
+            await ctx.reply(f"게임: {stream['game']}")
+        else:
+            await ctx.reply("방송중이 아닙니다.")
+
+    async def on_open(self, sock):
+        print(" [BC] Bot is ready")
+
+    async def on_data(self, ctx):
+        print(ctx.user.name, ctx.channel.name, ctx.message.type, ctx.message.message)
+
+    async def on_error(self, e):
+        print(f"\n\n\n    {repr(e)}\n\n\n")
+
+def setup(bot):
+    print("I was activated")
+    bot.add_cog(BasicCommands(bot))
---
+++ Twitch/cogs/console_logger.py
@@ -0,0 +1,17 @@
+class ConsoleLogger:
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = set()
+
+    async def on_open(self, sock):
+        print(" [CL] Bot is ready")
+
+    async def on_data(self, ctx):
+        print(f" [CL] {ctx.message.raw}")
+
+    async def on_error(self, e):
+        print(f" ! Exception {repr(e)}")
+
+
+def setup(bot):
+    bot.add_cog(ConsoleLogger(bot))
