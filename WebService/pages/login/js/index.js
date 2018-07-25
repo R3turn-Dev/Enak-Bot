681fe863f77f14ec9c3303e49d9783564acbd908
return0927 <bjleh0927@gmail.com>
2018-07-26 03:32:46 +0900

    * 2018.07.26 Added front-end pages
     - Added Root / Login / SSO webpages

---
+++ WebService/pages/login/js/index.js
@@ -0,0 +1,11 @@
+function twitch() {
+    window.open(twitch_uri, "_blank", "height=650, width=800");
+}
+
+function discord() {
+    window.open(discord_uri, "_blank", "height=650, width=800");
+}
+
+function success() {
+    location.href = "/";
+}
